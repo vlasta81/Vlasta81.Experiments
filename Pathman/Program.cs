f81b0a4451dbe5f8813478acec12671ef97d73c7
vlasta81 <vlasta81@gmail.com>
2024-07-28 22:19:37 +0200

    Create "Pathman" app and little bit fixed errors.

--- Pathman/Program.cs
+++ Pathman/Program.cs
@@ -1,67 +1,150 @@
-﻿using System.Security.Principal;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
 
 namespace Pathman
 {
-    internal class Program
+    internal static class Helpers
     {
         public static bool IsAdministrator()
         {
             return (new WindowsPrincipal(WindowsIdentity.GetCurrent())).IsInRole(WindowsBuiltInRole.Administrator);
         }
-        static void Main(string[] args)
+    }
+
+    internal class AppCommands
+    {
+        public static void AddPathCommand(string directoryPath, bool systemEnvironment, bool addToBeginning)
         {
-            if (args.Length < 2)
+            Console.WriteLine($"(i) Starting.");
+            string systemEnvironmentName = systemEnvironment ? "system" : "user";
+            EnvironmentVariableTarget environmentType = systemEnvironment ? EnvironmentVariableTarget.Machine : EnvironmentVariableTarget.User;
+            DirectoryInfo di = new DirectoryInfo(directoryPath);
+            Console.WriteLine($"(i) Getting \"Path\" {systemEnvironmentName} environment variable.");
+            List<string> paths = Environment.GetEnvironmentVariable("Path", environmentType).Split(';').ToList();
+            Console.WriteLine($"(i) Looking for directory path in \"Path\" {systemEnvironmentName} environment variable.");
+            if (paths.FindIndex(a => a.Contains(di.FullName)) == -1) // -1 == not found
             {
-                Console.WriteLine(Help.GetUseHelp());
-                return;
+                if (addToBeginning)
+                {                    
+                    Console.WriteLine($"(i) The directory path is added to the beginning {systemEnvironmentName} environment variable.");
+                    paths.Insert(0, di.FullName);
+                }
+                else
+                {
+                    Console.WriteLine($"(i) The directory path is added to the {systemEnvironmentName} environment variable.");
+                    paths.Add(di.FullName);
+                }
+                Console.WriteLine($"(i) The \"Path\" environment is stored.");
+                string newPaths = string.Join(";", paths.ToArray());
+                if (environmentType == EnvironmentVariableTarget.User)
+                {
+                    Environment.SetEnvironmentVariable("Path", newPaths, EnvironmentVariableTarget.User);
+                    Console.WriteLine($"(i) Done.");
+                }
+                else if (Helpers.IsAdministrator() && environmentType == EnvironmentVariableTarget.Machine)
+                {
+                    Environment.SetEnvironmentVariable("Path", newPaths, EnvironmentVariableTarget.Machine);
+                    Console.WriteLine($"(i) Done.");
+                }
+                else
+                {
+                    Console.WriteLine($"(e) You need administrator permissions to remove the directory path from the {systemEnvironmentName} environment variable!");
+                }
             }
-            if (!Directory.Exists(args[1].Trim()))
+            else
             {
-                Console.WriteLine("Directory does not exist!");
-                return;
-            }
-            bool AddToBeginning = (args.Length > 2 && args[2].Trim() == "-f") ? true : false;
-
-            var name = "PATH";
-            var scope = EnvironmentVariableTarget.Machine; // or User
-            var oldValue = Environment.GetEnvironmentVariable(name, scope);
-            var newValue = oldValue + @";C:\Program Files\MySQL\MySQL Server 5.1\bin\\";
-            Environment.SetEnvironmentVariable(name, newValue, scope);
-
-            switch (args[0])
-            {
-                case "-h":
-                case "-help":
-                    Console.Clear();
-                    Console.WriteLine(Help.GetHelp());
-                    break;
-                case "-s":
-                    if (args.Length > 1 && int.TryParse(args[1].Trim(), out int DecimalNumber))
-                    {
-                        Console.WriteLine("Decimal: " + DecimalNumber);
-                        Console.WriteLine(" Binary: " + Convert.ToString(DecimalNumber, 2));
-                    }
-                    else
-                    {
-                        Console.WriteLine(Help.GetUseHelp());
-                    }
-                    break;
-                case "-u":
-                    if (args.Length > 1 && Regex.IsMatch(args[1].Trim(), "^[01]{1,8}$"))
-                    {
-                        Console.WriteLine(" Binary: " + args[1].Trim());
-                        Console.WriteLine("Decimal: " + Convert.ToInt32(args[1].Trim(), 2).ToString());
-                    }
-                    else
-                    {
-                        Console.WriteLine(Help.GetUseHelp());
-                    }
-                    break;
-                default:
-                    Console.WriteLine(Help.GetUseHelp());
-                    break;
+                Console.WriteLine($"(w) Directory path is already exists in \"Path\" {systemEnvironmentName} environment variable.");
             }
         }
+        public static void RemovePathCommand(string directoryPath, bool systemEnvironment)
+        {
+            Console.WriteLine($"(i) Starting.");
+            string systemEnvironmentName = systemEnvironment ? "system" : "user";
+            EnvironmentVariableTarget environmentType = systemEnvironment ? EnvironmentVariableTarget.Machine : EnvironmentVariableTarget.User;
+            DirectoryInfo di = new DirectoryInfo(directoryPath);
+            Console.WriteLine($"(i) Getting \"Path\" {systemEnvironmentName} environment variable.");
+            List<string> paths = Environment.GetEnvironmentVariable("Path", environmentType).Split(';').ToList();
+            Console.WriteLine($"(i) Looking for directory path in \"Path\" {systemEnvironmentName} environment variable.");
+            int index = paths.FindIndex(a => a.Contains(di.FullName)); // -1 == not found
+            if (index == -1)
+            {
+                Console.WriteLine($"(w) Directory path does not exists in \"Path\" {systemEnvironmentName} environment variable.");
+            }
+            else
+            {
+                Console.WriteLine($"(i) The directory path is removed from the {systemEnvironmentName} environment variable.");
+                paths.RemoveAt(index);
+                Console.WriteLine($"(i) The \"Path\" environment is stored.");
+                string newPaths = string.Join(";", paths.ToArray());
+                if (environmentType == EnvironmentVariableTarget.User)
+                {
+                    Environment.SetEnvironmentVariable("Path", newPaths, EnvironmentVariableTarget.User);
+                    Console.WriteLine($"(i) Done.");
+                }
+                else if (Helpers.IsAdministrator() && environmentType == EnvironmentVariableTarget.Machine)
+                {
+                    Environment.SetEnvironmentVariable("Path", newPaths, EnvironmentVariableTarget.Machine);
+                    Console.WriteLine($"(i) Done.");
+                }
+                else
+                {
+                    Console.WriteLine($"(e) You need administrator permissions to remove the directory path from the {systemEnvironmentName} environment variable!");
+                }
+            }
+        }
+    }
+
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Option<string> directoryOption = new Option<string>("--directory", "The directory path that is added to the \"Path\" environment variable.");
+            directoryOption.IsRequired = true;
+            directoryOption.ArgumentHelpName = "path";
+            directoryOption.AddAlias("-d");
+
+            Option<bool> systemEnvironmentOption = new Option<bool>("--system", "Select if you want to add/remove the directory path to/from the system instead of user environment \"Path\" variable. (Required administrator permissions!)");
+            systemEnvironmentOption.SetDefaultValue(false);
+            systemEnvironmentOption.AddAlias("-s");
+
+            Option<bool> addToBeginningOption = new Option<bool>("--first", "Select if you want to add the directory path to the beginning of the \"Path\" environment variable.");
+            addToBeginningOption.SetDefaultValue(false);
+            addToBeginningOption.AddAlias("-f");
+
+            RootCommand rootCommand = new RootCommand("Pathman is a CLI tool managing the windows \"Path\" environment variable.");
+
+            Command addCommand = new Command("add", "Add path to environment variable.");
+            addCommand.AddOption(directoryOption);
+            addCommand.AddOption(systemEnvironmentOption);
+            addCommand.AddOption(addToBeginningOption);
+            addCommand.SetHandler(AppCommands.AddPathCommand, directoryOption, systemEnvironmentOption, addToBeginningOption);
+            rootCommand.Add(addCommand);
+
+            Command removeCommand = new Command("remove", "Remove path from environment variable.");
+            removeCommand.AddOption(directoryOption);
+            removeCommand.AddOption(systemEnvironmentOption);
+            removeCommand.SetHandler(AppCommands.RemovePathCommand, directoryOption, systemEnvironmentOption);
+            rootCommand.Add(removeCommand);
+
+            CommandLineBuilder commandLineBuilder = new CommandLineBuilder(rootCommand);
+
+            commandLineBuilder.AddMiddleware(async (context, next) =>
+            {
+                await next(context);
+            });
+
+            commandLineBuilder.UseDefaults();
+
+            Parser parser = commandLineBuilder.Build();
+
+            await parser.InvokeAsync(args);
+        }
+
     }
 }
--- Pathman/README.md
+++ Pathman/README.md
@@ -1,31 +1,65 @@
 ﻿# Pathman
 
 
-Manage Windows environment variable "Path".
+Pathman is a CLI tool managing the windows "Path" environment variable.
 
 
 #### USING
+### Commands
 
-    Pathman -s [path]
-        Add path to system variable "Path". 
-        ! Requires administrator rights.
-        [path] can be only absolute path to directory
+    Usage:
+      Pathman [command] [options]
 
-    Pathman -u [path]
-        Add path to user variable "Path".
-        [path] can be only absolute path to directory
+    Options:
+      --version       Show version information
+      -?, -h, --help  Show help and usage information
 
-    -b
-        Add path to beginning variable "Path"
+    Commands:
+      add     Add path to environment variable.
+      remove  Remove path from environment variable.
 
-    Pathman -h [or] -help
-        Show this help.
+### Command "add"
+
+    Description:
+      Add path to environment variable.
+
+    Usage:
+      Pathman add [options]
+
+    Options:
+      -d, --directory <path> (REQUIRED)  The directory path that is added to the "Path" environment variable.
+      -s, --system                       Select if you want to add/remove the directory path to/from the system instead of
+                                         user environment "Path" variable. (Required administrator permissions!) [default:
+                                         False]
+      -f, --first                        Select if you want to add the directory path to the beginning of the "Path"
+                                         environment variable. [default: False]
+      -?, -h, --help                     Show help and usage information
+
+### Command "remove"
+
+    Description:
+      Remove path from environment variable.
+
+    Usage:
+      Pathman remove [options]
+
+    Options:
+      -d, --directory <path> (REQUIRED)  The directory path that is added to the "Path" environment variable.
+      -s, --system                       Select if you want to add/remove the directory path to/from the system instead of
+                                         user environment "Path" variable. (Required administrator permissions!) [default:
+                                         False]
+      -?, -h, --help                     Show help and usage information
+
 
 #### EXAMPLES
 
-    Pathman -s "C:\Path\To Directory"
-    Pathman -u C:\Path\To\Directory
-    Pathman -s "C:\Path\To Directory" -b
-    Pathman -u C:\Path\To\Directory -b
+    Pathman add "C:\Path\To Directory" --system
+    Pathman add "C:\Path\To Directory" --system --first
+    Pathman add C:\Path\To\Directory
+    Pathman add "C:\Path\To Directory" -f
+    Pathman add C:\Path\To\Directory --first
+    Pathman remove "C:\Path\To Directory" -s
+    Pathman remove C:\Path\To\Directory
+    Pathman -?
     Pathman -h
     Pathman -help
